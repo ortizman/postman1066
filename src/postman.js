a9478111994b4c3f23da0a17396fadcb350142f7
Manuel Ortiz <ortizman@gmail.com>
2021-12-09 23:32:44 -0300

    remove code comment

--- src/postman.js
+++ src/postman.js
@@ -3,10 +3,8 @@ const backURL = process.env.REACT_APP_BACK_URL || 'https://post1066.herokuapp.co
 
 export default async function getCollection(collectionUrl) {
     if (useMock) {
-        // await sleep(3000)
         return require('./dummy.json');
     } else {
-        //return fetch(`https://api.getpostman.com/collections/${collectionUID}?apikey=${apiKey}`).then(res => res.json());
         return fetch(backURL + '/collection?url=' + collectionUrl)
             .then(res => res.json())
             .catch(err => console.log);
